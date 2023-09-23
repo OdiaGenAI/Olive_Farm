3525a0e573e9665ea4ef8a3452d0fc34cdb675a6
arkamaldeen <kabdurrahmaan28@gmail.com>
2023-09-24 00:41:25 +0530

    Updated app.py

--- custom_prompt_template.py
+++ custom_prompt_template.py
@@ -16,8 +16,28 @@ class InstructionGenerationTemplate(langchain.prompts.PromptTemplate):
 
         Please generate the instructions in the {instruction_format} format and in {lang} language. Remember to adhere to the rules mentioned above.
     """
+
     template_format = "f-string" 
     def format(self, **kwargs):
         """Format the prompt."""
         return self.template.format(**kwargs)
+    
+class AnswerGenerationTemplate(langchain.prompts.PromptTemplate):
+    """A custom prompt template for generating answers to questions."""
+    
+    input_variables: List[str] = ["questions", "additional_rules"]
+    
+    template = """
+        You are a highly intelligent language model tasked with providing answers to the following questions :
 
+        Questions:
+        {questions}
+
+        Please follow these rules:
+        {additional_rules}
+    """
+
+    template_format = "f-string" 
+    def format(self, **kwargs):
+        """Format the prompt."""
+        return self.template.format(**kwargs)
